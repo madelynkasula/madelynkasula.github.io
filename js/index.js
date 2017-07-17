ea46bc7b703987fbb3bae9a3c64ad207e143323d
Madelyn Kasula <madelynkasula@gmail.com>
2017-07-16 21:15:27 -0500

    Add select javascript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-  
+  $('#info').click(function() {
+    this.prepend('do it');
+  });
+
+  $(document).keypress(function(e) {
+    var selectedEl = document.getElementsByClassName('select')[0];
+
+    switch(e.key) {
+      case 's':
+        selectNext(selectedEl);
+        break;
+      case 'w':
+        selectPrevious(selectedEl);
+        break;
+    }
+  });
+
+  function selectNext(el) {
+    let nextEl = $(el).next();
+    toggleSelectClass(el, nextEl);
+  }
+
+  function selectPrevious(el) {
+    let prevEl = $(el).prev();
+    toggleSelectClass(el, prevEl);
+  }
+
+  function toggleSelectClass(oldEl, newEl) {
+    if (newEl.length != 0) {
+      $(oldEl).removeClass('select');
+      $(newEl).addClass('select');
+    }
+  }
 });
