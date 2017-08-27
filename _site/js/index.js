325177db6fb77289c1b5bb3758d7718fb1e55835
Madelyn Kasula <madelynkasula@gmail.com>
2017-08-27 15:55:17 -0500

    Add scrollTo for sections

--- _site/js/index.js
+++ _site/js/index.js
@@ -1,16 +1,28 @@
 $(document).ready(() => {
-  const offset = 100
-  const duration = 300
+  const offset = 75
+  const sectionOffset = 45
+  const duration = 500
 
   $(window).scroll(() => {
     if ($(this).scrollTop() > offset) {
       $('#up-arrow').fadeIn(duration)
     } else {
-      $('#up-arrow').fadeOut(duration)
+      $('#up-arrow').fadeOut(0)
     }
   })
 
   $('#up-arrow').click((event) => {
     $('html, body').animate({ scrollTop: 0 }, duration)
+  })
+
+  scrollTo = (event, id) => {
+    event.preventDefault()
+    const scrollTop = $(id).position().top + sectionOffset
+    $('html, body').animate({ scrollTop }, duration)
+  }
+
+  $('.links > a').click((event) => {
+    const scrollTarget = event.target.hash
+    scrollTo(event, scrollTarget)
   })
 })
